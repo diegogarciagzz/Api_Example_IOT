c98a6b40fc43381a6c1c01da8ec2c21fcfac4727
Ernesto Cantu <ernesto.cantu@aleph5.com>
2024-10-14 12:41:03 -0600

    local database

--- database/db.js
+++ database/db.js
@@ -1,8 +1,9 @@
 const mysql = require('mysql2');
 var fs = require("fs");
-require('dotenv').config() // https://dev.to/_staticvoid/accessing-env-files-natively-with-nodejs-44hf - ACCESS TO ENV Vars
+const constants = require("../constants")
+//require('dotenv').config() // https://dev.to/_staticvoid/accessing-env-files-natively-with-nodejs-44hf - ACCESS TO ENV Vars
 
-const {DB_HOST, DB_PORT,DB_USER, DB_PASS} = process.env
+//const {DB_HOST, DB_PORT,DB_USER, DB_PASS} = process.env
 
 /**
  * Método que configura un objeto conexión y lo regresa a quien lo solicite.
@@ -16,15 +17,15 @@ const {DB_HOST, DB_PORT,DB_USER, DB_PASS} = process.env
  */
 function getConnection(){
   const connection = mysql.createConnection({
-    host: DB_HOST,
-    user: DB_USER,
-    port: DB_PORT,
-    password: DB_PASS,
+    host: constants.dbHost,
+    user: constants.dbUser,
+    port: constants.dbPort,
+    password: constants.dbPass,
     database: "iot",
     dateStrings: true, //https://stackoverflow.com/questions/49475282/mysql-date-different-while-retrieving-from-node-js - Ignore timezone and use dates as strings
-    ssl: {
-      ca: fs.readFileSync('C:/Conciencia/Projects/ca_tec.pem')
-    }
+    // ssl: {
+    //   ca: fs.readFileSync('C:/Conciencia/Projects/ca_tec.pem')
+    // }
   });
 
   return connection;
